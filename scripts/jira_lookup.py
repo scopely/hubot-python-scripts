f84310010803dca22661c4370b3f03fd3842ba0f
Max Goedjen <max.goedjen@gmail.com>
2014-01-23 14:48:50 -0800

    Fix issue with jira where no there would be no response if the issue object didn't have an assignee attr

--- scripts/jira_lookup.py
+++ scripts/jira_lookup.py
@@ -24,8 +24,9 @@ HOST = environ.get('HUBOT_JIRA_HOSTNAME', '')
 USERNAME = environ.get('HUBOT_JIRA_USERNAME', '')
 PASSWORD = environ.get('HUBOT_JIRA_PASSWORD', '')
 
+
 class JIRALookup(HubotScript):
-    
+
     @hear('([a-z]{2,100}-[0-9]+)')
     def lookup_jira(self, message, matches):
         issue_id = matches[0]
@@ -35,10 +36,14 @@ class JIRALookup(HubotScript):
             url = '{base}/browse/{id}'.format(base=HOST, id=issue_id)
             desc = ''
             env = ''
+            assignee = 'unassigned'
             if issue.fields.description:
                 desc = issue.fields.description
             if issue.fields.environment:
                 env = '\n{0}'.format(issue.fields.environment)
-            return '{id}: {status}, assigned to {assignee} ({tags})\n{title}: {url}\n{desc}{env}'.format(id=issue_id.upper(), status=issue.fields.status.name, title=issue.fields.summary, desc=desc,url=url, tags=', '.join(issue.fields.labels), assignee=issue.fields.assignee.displayName, env=env)
+            if hasattr(issue, 'assignee'):
+                assignee = 'assigned to {assignee}'.format(
+                    assignee=issue.assignee.displayName)
+            return '{id}: {status}, {assignee} ({tags})\n{title}: {url}\n{desc}{env}'.format(id=issue_id.upper(), status=issue.fields.status.name, title=issue.fields.summary, desc=desc, url=url, tags=', '.join(issue.fields.labels), assignee=assignee, env=env)
         except Exception as e:
             pass
